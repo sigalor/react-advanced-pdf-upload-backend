ba3f1a366205e44d5e79fb4c0696370524445c1c
sigalor <nalenz@protonmail.com>
2023-03-02 17:39:08 +0100

    chore: rebuild library

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="node" />
-export declare type RotateModification = {
+export type RotateModification = {
     type: 'rotate';
     degrees: -90 | 90;
 };
-export declare type Modification = RotateModification;
-export declare type BuildPdfData = {
+export type Modification = RotateModification;
+export type BuildPdfData = {
     files: (string | Buffer)[];
     pages: {
         origin: {
@@ -15,11 +15,11 @@ export declare type BuildPdfData = {
     }[];
     name?: string;
 };
-export declare type RenderPdfData = {
+export type RenderPdfData = {
     file: string | Buffer;
     resolution: number;
 };
-export declare type RenderPdfOutput = {
+export type RenderPdfOutput = {
     pages: {
         uri: string;
         dimensions: {
